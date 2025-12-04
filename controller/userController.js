03ac208464e4238ad1ae135e99be884146835566
Manu22-cloud-cell <mailtomanojky@gmail.com>
2025-12-04 17:08:06 +0530

    Encrypting user password using bcrpt  pacakage

--- controller/userController.js
+++ controller/userController.js
@@ -1,20 +1,35 @@
 const Users=require('../models/users');
+const bcrypt=require('bcryptjs');
 
 const userSignUp = async (req,res)=>{
 
     try {
     const {userName,email,password}=req.body;
+
+    //check if user already exist
+    const existingUser=await Users.findOne({where:{email}});
+
+    if(existingUser){
+        return res.status(409).json({message:"Email already registered"});
+    }
+
+    //Hash password
+    const hashedPassword = await bcrypt.hash(password,10);
+
     const newUser=await Users.create({
         userName:userName,
         email:email,
-        password:password
+        password:hashedPassword
     });
+
     console.log("New User account is created");
     res.status(201).json({message:"User created succuessfully",newUser})
+
     } catch (error) {
+        console.log.apply(error);
         res.status(409).send({error:"Email already registered"}); 
     }
-}
+};
 
 const userLogin= async (req,res)=>{
     try {
@@ -26,7 +41,9 @@ const userLogin= async (req,res)=>{
             return res.status(404).json({message:"User not found"});
         }
 
-        if(user.password !==password) {
+        const isMatch=await bcrypt.compare(password,user.password);
+
+        if(!isMatch) {
             return res.status(401).json({message:"Incorrect password"});
         }
         res.status(200).json({message:"Login Successful",user});
