c20de4b82bed430058a2c3ab182e872e32966d18
Manu22-cloud-cell <mailtomanojky@gmail.com>
2025-12-29 12:59:44 +0530

    Created new ec2 instance and so replaced old public ip with new ip.

--- Frontend/addExpenses/expense.js
+++ Frontend/addExpenses/expense.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://16.112.63.99";
+const API_BASE_URL = "http://16.112.58.170";
 
 let currentPage = 1;
 let ITEMS_PER_PAGE = Number(localStorage.getItem("itemsPerPage")) || 10;
--- Frontend/reports/reports.js
+++ Frontend/reports/reports.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://16.112.63.99";
+const API_BASE_URL = "http://16.112.58.170";
 
 const isPremium = localStorage.getItem("isPremium");
 
